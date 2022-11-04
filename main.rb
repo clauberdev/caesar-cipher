fda114fc2dc0b25ed64f5d9edb368a31a58c24b8
Clauber <clauber.sportmg@gmail.com>
2022-11-04 14:41:27 -0300

    caesar cipher

---
+++ main.rb
@@ -0,0 +1,22 @@
+
+def caesar_cipher(text, shift)
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    text.downcase!
+    res = ""
+
+    alphabet_len = alphabet.length - 1
+    for i in 0..text.length - 1
+        if text[i] == ' ' || text[i] == '!'
+            res += text[i]
+        end
+        for j in 0..alphabet_len
+            if text[i] == alphabet[j]
+                res += alphabet[( shift - (alphabet_len - j) ) - 1]
+            end
+        end
+    end
+
+    return res.capitalize
+end
+
+puts caesar_cipher("What a string!", 5) # "Bmfy f xywnsl!"
